0c161a9700ebe87dc65da1c56910e59e39a38a52
JagadheeshAvu <jagadheesh6349@gmail.com>
2023-04-01 07:27:19 +0000

    Notifications Application Solution React JS

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 const Notification = (props) => {
-  //  Write your code here.
-  const { className, messageText } = props;
+  const { imgUrl, messageText } = props;
   return (
     <div>
-      <img src={imageUrl} className="icon" />
-      <p className={`notification ${className}`}>{messageText}</p>
+      <img src={imgUrl} className="icon" />
+      <p className="message">{messageText}</p>
     </div>
   );
+  //  Write your code here.
 };
 
 const element = (
   //  Write your code here.
   <div className="bg-container">
     <h1 className="notifications-heading">Notifications</h1>
-    <div className="notification-container">
-      <Notification
-        className="message"
+    <div className="primary-container">
+      <notification
         messageText="Information Message"
-        imageUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/primary-icon-img.png"
+      />
+    </div>
+    <div className="success-container">
+      <notification
+        messageText="Success Message"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/success-icon-img.png"
+      />
+    </div>
+    <div className="warning-container">
+      <notification
+        messageText="Warning Message"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/warning-icon-img.png"
+      />
+    </div>
+    <div className="danger-container">
+      <notification
+        messageText="Error Message"
+        imgUrl="https://assets.ccbp.in/frontend/react-js/danger-icon-img.png"
       />
     </div>
   </div>
